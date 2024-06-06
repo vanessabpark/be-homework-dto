3a161f8a28ab1bae952c8dab9a6a9214ed0c0f9c
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-06-06 21:38:02 +0900

    Add files via upload

---
+++ src/main/java/com/springboot/Section3Week1HomeworkDtoApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3Week1HomeworkDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section3Week1HomeworkDtoApplication.class, args);
+	}
+
+}
